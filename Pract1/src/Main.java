de6d7a3237175243f4bbb24bd9498db9c0eb4ebb
Xatsu-52 <fedosovkosticlo.ru@gmail.com>
2026-09-24 21:54:39 +0300

    Complite integral task

--- Pract1/src/Main.java
+++ Pract1/src/Main.java
@@ -1,16 +1,159 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static final int SIZE = 10000000; 
+    public static final int THREADS = 6;
+    public static final int ITEMS_PER_THREAD = SIZE / THREADS;
+    public static final double A = 0.0;
+    public static final double B = Math.PI;
+    public static final double H = (B - A) / SIZE;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    //f(x) = sin(x)
+    public static double f(double x) {
+        return Math.sin(x);
+    }
+
+    static class Acc {
+        volatile double acc = 0.0;
+        synchronized public void addToAcc(double n) {
+            acc += n;
         }
-        System.out.println("Program is ended");
+    }
+
+    public static Thread taskThread(int n, int[] schedule, double[] results) {
+        return new Thread(() -> {
+            int start = schedule[n];
+            int finish = schedule[n] + ITEMS_PER_THREAD;
+            double sum = 0.0;
+            for (int i = start; i < finish; i++) {
+                double x = A + (i + 0.5) * H;
+                sum += f(x) * H;
+            }
+            results[n] = sum;
+        });
+    }
+
+    public static Thread taskMonitorThread(int n, int[] schedule, Acc acc) {
+        return new Thread(() -> {
+            int start = schedule[n];
+            int finish = schedule[n] + ITEMS_PER_THREAD;
+            double sum = 0.0;
+            for (int i = start; i < finish; i++) {
+                double x = A + (i + 0.5) * H;
+                sum += f(x) * H;
+            }
+            acc.addToAcc(sum);
+        });
+    }
+
+    public static Thread taskAtomicThread(int n, int[] schedule, java.util.concurrent.atomic.DoubleAccumulator acc) {
+        return new Thread(() -> {
+            int start = schedule[n];
+            int finish = schedule[n] + ITEMS_PER_THREAD;
+            double sum = 0.0;
+            for (int i = start; i < finish; i++) {
+                double x = A + (i + 0.5) * H;
+                sum += f(x) * H;
+            }
+            acc.accumulate(sum);
+        });
+    }
+
+    public static void measureSequential() {
+        var start = System.nanoTime();
+        double sum = 0.0;
+        for (int i = 0; i < SIZE; i++) {
+            double x = A + (i + 0.5) * H;
+            sum += f(x) * H;
+        }
+        var finish = System.nanoTime();
+        System.out.println("Sequential result");
+        System.out.println(sum);
+        System.out.println("Sequential time (ms)");
+        System.out.println((double) (finish - start) / 1000000);
+    }
+
+    public static void measureP() throws InterruptedException {
+        var threadsStart = new int[THREADS];
+        var threads = new Thread[THREADS];
+        for (int i = 0; i < THREADS; i++) {
+            threadsStart[i] = i * ITEMS_PER_THREAD;
+        }
+        double[] results = new double[THREADS];
+
+        var pStart = System.nanoTime();
+        for (int i = 0; i < THREADS; i++) {
+            threads[i] = taskThread(i, threadsStart, results);
+        }
+        for (int i = 0; i < THREADS; i++)
+            threads[i].start();
+        for (int i = 0; i < THREADS; i++)
+            threads[i].join();
+
+        double pResult = 0.0;
+        for (int i = 0; i < THREADS; i++) {
+            pResult += results[i];
+        }
+        var pFinish = System.nanoTime();
+        System.out.println("Parallel result");
+        System.out.println(pResult);
+        System.out.println("Parallel time (ms)");
+        System.out.println((double) (pFinish - pStart) / 1000000);
+    }
+
+    public static void measureMon() throws InterruptedException {
+        var threadsStart = new int[THREADS];
+        var threads = new Thread[THREADS];
+        for (int i = 0; i < THREADS; i++) {
+            threadsStart[i] = i * ITEMS_PER_THREAD;
+        }
+
+        var pStart = System.nanoTime();
+        Acc acc = new Acc();
+        for (int i = 0; i < THREADS; i++) {
+            threads[i] = taskMonitorThread(i, threadsStart, acc);
+        }
+        for (int i = 0; i < THREADS; i++)
+            threads[i].start();
+        for (int i = 0; i < THREADS; i++)
+            threads[i].join();
+
+        double pResult = acc.acc;
+        var pFinish = System.nanoTime();
+        System.out.println("Monitor result");
+        System.out.println(pResult);
+        System.out.println("Monitor time (ms)");
+        System.out.println((double) (pFinish - pStart) / 1000000);
+    }
+
+    public static void measureAtomic() throws InterruptedException {
+        var threadsStart = new int[THREADS];
+        var threads = new Thread[THREADS];
+        for (int i = 0; i < THREADS; i++) {
+            threadsStart[i] = i * ITEMS_PER_THREAD;
+        }
+
+        var pStart = System.nanoTime();
+        var acc = new java.util.concurrent.atomic.DoubleAccumulator(Double::sum, 0.0);
+        for (int i = 0; i < THREADS; i++) {
+            threads[i] = taskAtomicThread(i, threadsStart, acc);
+        }
+        for (int i = 0; i < THREADS; i++)
+            threads[i].start();
+        for (int i = 0; i < THREADS; i++)
+            threads[i].join();
+
+        double pResult = acc.get();
+        var pFinish = System.nanoTime();
+        System.out.println("Atomic (DoubleAccumulator) result");
+        System.out.println(pResult);
+        System.out.println("Atomic (DoubleAccumulator) time (ms)");
+        System.out.println((double) (pFinish - pStart) / 1000000);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Integration benchmark: f(x) = sin(x) from " + A + " to " + B + " with " + SIZE + " steps, " + THREADS + " threads.");
+        measureSequential();
+        measureP();
+        measureAtomic();
+        measureMon();
     }
 }
